2bdceb16438456e079a39eed1ad69108f7ed0886
devbot5911 <devbot5911@gmail.com>
2025-05-04 18:08:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -44,7 +44,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/devbot5911/VillainMusic/tree/master",
+    "https://github.com/devbot5911/VillainMusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
